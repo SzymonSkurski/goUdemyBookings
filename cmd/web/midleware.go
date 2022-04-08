c146c64a1f4a87a7f2f6e6af82ae635a57e04088
Szymon Skurski <szymon.skurski@gmail.com>
2022-04-08 12:33:49 +0200

    add gitignore

--- cmd/web/midleware.go
+++ cmd/web/midleware.go
@@ -29,5 +29,6 @@ func NoSurf(next http.Handler) http.Handler {
 
 // SessionLoad ensure to load session on each request
 func SessioLoad(next http.Handler) http.Handler {
+	
 	return session.LoadAndSave(next)
 }
